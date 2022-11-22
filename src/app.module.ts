32a7e1fd5c038f78f13c9b9f284a48118a38dfd6
pbgikuna <pbt0010+7@gmail.com>
2022-11-22 15:00:41 +0400

    added sqlite db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountriesModule } from './countries/countries.module';
 import { StatisticsModule } from './statistics/statistics.module';
 import { AuthModule } from './auth/auth.module';
-
 @Module({
-  imports: [CountriesModule, StatisticsModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    CountriesModule,
+    StatisticsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
